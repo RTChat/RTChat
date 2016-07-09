e4b4adc9457c5df4ce6b5d5654114626eb40e8bb
Jon Knapp <burtonboarder8@gmail.com>
2016-07-09 01:40:06 -0700

    WIP

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,18 +1,18 @@
 exports.config = {
-  // seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: [
-  	'test/e2e/*.js'
-  ],
+	// seleniumAddress: 'http://localhost:4444/wd/hub',
+	specs: [
+		'test/e2e/*.js'
+	],
 
-  capabilities: {
-	  'browserName': 'chrome',
-	  'chromeOptions': {
-	    'args': [
-	    	// 'show-fps-counter=true',
-	    	// 'console',
-	    	'use-fake-device-for-media-stream',
-	    	'use-fake-ui-for-media-stream',
-	    ]
-	  }
+	capabilities: {
+		'browserName': 'chrome',
+		'chromeOptions': {
+			'args': [
+				// 'console',
+				'no-sandbox',
+				'use-fake-device-for-media-stream',
+				'use-fake-ui-for-media-stream',
+			]
+		}
 	},
 };
