16102208812d3934dff623e8fba7197dc8488627
Jon Knapp <burtonboarder8@gmail.com>
2016-08-05 01:10:37 -0700

    Improved e2e tests

---
+++ test/e2e/chat_panel.js
@@ -0,0 +1,46 @@
+
+describe("Chat Panel", function() {
+
+	beforeEach(function() {
+		browser.get( browser.baseUrl + "#testing");
+		browser.waitForRender();
+	});
+
+	describe("with a second user", function() {
+
+
+		beforeEach(function() {
+			browser2 = browser.fork();
+
+			browser2.wait(function() {
+				return browser2.$('.users-panel li').isPresent();
+			});
+
+			browser.wait(function () {
+				return browser.$('.users-panel li').isPresent();
+			});
+		});
+
+		afterEach(function() {
+			browser2.quit();
+		});
+
+
+		it("should show chats to eachother.", function() {
+			browser.$('.emojionearea-editor').sendKeys("I'm number one! :cat:\n\n");
+
+			browser2.wait(function() {
+				return browser.$$('#ChatPanel li').get(0).getAttribute('innerHTML').toMatch("I'm number one!")
+			});
+
+			browser2.$('.emojionearea-editor').sendKeys("I'm number two! :cat2:\n\n");
+
+			browser2.wait(function() {
+				return browser.$$('#ChatPanel li').get(0).getAttribute('innerHTML').toMatch("I'm number one!")
+			});
+		});
+
+		//TODO:
+		// it("should sync history.", function() {});
+	});
+});
--- test/e2e/rtc_wrapper.js
+++ test/e2e/rtc_wrapper.js
@@ -1,61 +1,41 @@
 
-function waitForRender(brwsr) {
-	//TODO: reliable?
-	(brwsr || browser).wait(function() {
-		return $('body > *').isPresent()
-	}, 2000);
-}
-
-function forkBrowser(url) {
-	var new_browser = browser.forkNewDriverInstance(url || true);
-	new_browser.ignoreSynchronization = true;
-
-	if (url) new_browser.get(url);
-	return new_browser;
-}
-
-browser.ignoreSynchronization = true;
-
 describe("RTC Wrapper", function() {
 
-	var BaseUrl = "https://localhost:9001";
-
 	beforeEach(function() {
-		browser.get( BaseUrl + "#testing");
-		waitForRender();
+		browser.get( browser.baseUrl + "#testing");
+		browser.waitForRender();
 	});
 
 	describe("RoomPanel", function() {
 
 		it('should render alone', function() {
 			// users panel should be empty.
-			expect($('.users-panel').isPresent()).toBeTruthy();
+			browser.wait(function () {
+				return browser.$('.users-panel').isPresent();
+			});
 			expect($('.users-panel li').isPresent()).toBeFalsy();
 		});
 
-		describe("with another user", function() {
+		describe("with a second user", function() {
 			var browser2;
 
 			beforeEach(function() {
-				browser2 = forkBrowser();
+				browser2 = browser.fork();
 
 				browser2.wait(function() {
 					return browser2.$('.users-panel li').isPresent();
-				}, 2000);
+				});
 
 				browser.wait(function () {
 					return browser.$('.users-panel li').isPresent();
-				}, 2000);
+				});
 			});
 
 			afterEach(function() {
 				browser2.quit();
 			});
 
-			it("should display that other user", function() {
-				expect(browser.$$( '.users-panel li').count()).toEqual(1);
-				expect(browser2.$$('.users-panel li').count()).toEqual(1);
-
+			it("should display that second user", function() {
 				var b1Name = browser.$$( '#UserMenu span').first().getAttribute('innerHTML');
 				var b2Name = browser2.$$('#UserMenu span').first().getAttribute('innerHTML');
 
@@ -68,12 +48,149 @@ describe("RTC Wrapper", function() {
 
 			describe("that leaves", function() {
 				beforeEach(function() {
-					browser2.$$('#Header a').first().click();
-					//TODO: wait?
+					browser2.executeScript(function() {
+						window.location = "#"
+					})
+					browser2.wait(function() {
+						return browser2.$('#WelcomePanel .btn').isPresent();
+					});
 				});
 
-				it('should remove that user', function() {
- 					expect(browser.$$( '.users-panel li').count()).toEqual(0);
+				it('should remove the second user', function() {
+					expect(browser.$$( '.users-panel li').count()).toEqual(0);
+				});
+
+			});
+
+			describe("that updates the state", function() {
+
+				beforeEach(function() {
+					browser2.actions().mouseMove(browser2.$('.editor')).perform();
+					browser2.$('#RoomPanel .room-subject .btn').click();
+					browser2.$('#RoomPanel .room-subject .editor').sendKeys(
+						protractor.Key.chord(protractor.Key.CONTROL, "a")
+					);
+					browser2.$('#RoomPanel .room-subject .editor').sendKeys('second user rulez :fire: :cat:');
+					browser2.$('#RoomPanel .room-subject .btn').click();
+
+					browser.wait(function() {
+						return browser.$('.editor').getAttribute('innerHTML').toMatch("second user rulez");
+					}, 2000, "should sync the updated state");
+				});
+
+
+				describe("and leaves for another room", function() {
+					beforeEach(function() {
+						browser2.executeScript(function() {
+							window.location = "#testing2"
+						});
+						browser2.wait(function() {
+							return browser2.$('.users-panel').isPresent();
+						});
+					});
+
+					it('should remove the second user', function() {
+						expect(browser.$$( '.users-panel li').count()).toEqual(0);
+					});
+
+					it('the second user should have a fresh room', function() {
+						browser2.wait(function() {
+							return browser2.$('#RoomPanel .room-subject .editor').getAttribute('innerHTML').toMatch("Welcome to #testing2");
+						}, 2000, "should reset the state");
+						expect($('.users-panel li').isPresent()).toBeFalsy();
+					});
+
+					describe("before a third user joins", function() {
+						var browser3;
+
+						beforeEach(function() {
+							browser3 = browser.fork();
+
+							browser3.wait(function() {
+								return browser3.$$('.users-panel li').count().then(function(count) {
+									return count == 1;
+								});
+							});
+
+							browser.wait(function () {
+								return browser.$$('.users-panel li').count().then(function(count) {
+									return count == 1;
+								});
+							});
+						});
+
+						it("should display that third user", function() {
+							expect(browser.$$( '.users-panel li').count()).toEqual(1);
+							expect(browser3.$$('.users-panel li').count()).toEqual(1);
+
+							var b1Name = browser.$$( '#UserMenu span').first().getAttribute('innerHTML');
+							var b3Name = browser3.$$('#UserMenu span').first().getAttribute('innerHTML');
+
+							expect(b1Name).toContain("Guest_");
+							expect(b3Name).toContain("Guest_");
+
+							expect(browser.$$( '.users-panel li').first().getAttribute("innerHTML")).toContain(b3Name);
+							expect(browser3.$$( '.users-panel li').first().getAttribute("innerHTML")).toContain(b1Name);
+						});
+
+						afterEach(function() {
+							browser3.quit();
+						});
+					});
+
+				});
+			});
+
+			describe("and a third user", function() {
+				var browser3;
+
+				beforeEach(function() {
+					browser3 = browser.fork();
+
+					browser3.wait(function() {
+						return browser3.$$('.users-panel li').count().then(function(count) {
+							return count == 2;
+						});
+					}, 23000);
+
+					browser2.wait(function() {
+						return browser2.$$('.users-panel li').count().then(function(count) {
+							return count == 2;
+						});
+					}, 2200);
+
+					browser.wait(function () {
+						return browser.$$('.users-panel li').count().then(function(count) {
+							return count == 2;
+						});
+					}, 2100);
+				});
+
+				afterEach(function() {
+					browser3.quit();
+				});
+
+				it("should display that third user", function() {
+					expect(browser.$$( '.users-panel li').count()).toEqual(2);
+					expect(browser2.$$('.users-panel li').count()).toEqual(2);
+					expect(browser3.$$('.users-panel li').count()).toEqual(2);
+
+					var b1Name = browser.$$( '#UserMenu span').first().getAttribute('innerHTML');
+					var b2Name = browser2.$$('#UserMenu span').first().getAttribute('innerHTML');
+					var b3Name = browser3.$$('#UserMenu span').first().getAttribute('innerHTML');
+
+					expect(b1Name).toContain("Guest_");
+					expect(b2Name).toContain("Guest_");
+					expect(b3Name).toContain("Guest_");
+
+					expect(browser.$$( '.users-panel li').first().getAttribute("innerHTML")).toContain(b2Name);
+					expect(browser.$$( '.users-panel li').get(1).getAttribute("innerHTML")).toContain(b3Name);
+
+					expect(browser2.$$( '.users-panel li').first().getAttribute("innerHTML")).toContain(b1Name);
+					expect(browser2.$$( '.users-panel li').get(1).getAttribute("innerHTML")).toContain(b3Name);
+
+					expect(browser3.$$( '.users-panel li').first().getAttribute("innerHTML")).toContain(b1Name);
+					expect(browser3.$$( '.users-panel li').get(1).getAttribute("innerHTML")).toContain(b2Name);
 				});
 
 			});
